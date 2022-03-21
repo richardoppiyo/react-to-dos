1112da307553461391cb975ba0d78ed68a46ca68
Richard Opiyo <romenyo@dewcis.com>
2022-03-22 01:31:05 +0300

    fix linter issues

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,11 +1,14 @@
-import React from "react"
-import TodoItem from "./TodoItem";
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import TodoItem from './TodoItem';
 
 class TodosList extends React.Component {
   render() {
     return (
       <ul>
-        {this.props.todos.map(todo => (
+        {this.props.todos.map((todo) => (
           <TodoItem
             key={todo.id}
             todo={todo}
@@ -15,8 +18,8 @@ class TodosList extends React.Component {
           />
         ))}
       </ul>
-    )
+    );
   }
 }
 
-export default TodosList
+export default TodosList;
